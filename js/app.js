331f0020ecb27886de5aaf79307c36283972596e
Drake Buentello <dbuentello211607@gmail.com>
2022-10-22 22:23:24 -0500

    fixed js to show main after introduction is scrolled

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
 // function for hiding the introduction text, then
 // showing main content on user scroll
 
-// detects scroll from user
-window.onscroll = () => {
-    let userScroll = false;
-    if (userScroll == false) {
-        const introElement = document.querySelector('.introduction-container');
-        const mainElement = document.querySelector('.main-container-grid');
-        // remove show class, add hide class to intro div
-        introElement.classList.remove("show");
-        introElement.classList.add("hide-intro");
-        // remove hide class, add show class to main div
-        mainElement.classList.remove("hide-main");
-        mainElement.classList.add("show");
-        console.log(mainElement);
-    } else {
-        userScroll = true;
-    }
+window.onload = () => {
+    window.document.addEventListener('scroll', introductionHide);
 }
+
+// detects scroll from user
+function introductionHide() {
+    // get classes of respected elements
+    const introElement = document.querySelector('.introduction-container');
+    const mainElement = document.querySelector('.main-container-grid');
+    // remove show class, add hide class to intro div
+    introElement.classList.add("hide-intro");
+    // remove hide class, add show class to main div
+    mainElement.classList.remove("hide-main");
+    // this removes the event listener from window
+    window.document.removeEventListener('scroll', introductionHide);
+}
+
+
+
