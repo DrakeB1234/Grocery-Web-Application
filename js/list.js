f86d20bb7d94d7f4d4d7c8d2accbfd5f3b27221e
Drake Buentello <dbuentello211607@gmail.com>
2022-10-28 13:02:11 -0500

    added functionality to manage containers (js included)

--- js/list.js
+++ js/list.js
@@ -11,3 +11,26 @@ document.getElementById('add-button').addEventListener('click', () => {
     addButton.classList.add('rotate');
     addContainer.style.display = 'flex';
 });
+
+// hide/show manage container
+document.getElementById('manage-button').addEventListener('click', () => {
+    const manageContainer = document.getElementById('manage-container');
+    const manageButton = document.getElementById('manage-button');
+    if (manageContainer.style.display == 'block') {
+        manageContainer.style.display = 'none';
+        manageButton.classList.remove('rotate');
+        return;
+    }
+    manageButton.classList.add('rotate');
+    manageContainer.style.display = 'block';
+});
+
+// hide/show add container
+document.getElementById('add-category-button').addEventListener('click', () => {
+    const addContainer = document.getElementById('form-add-category');
+    if (addContainer.style.display == 'flex') {
+        addContainer.style.display = 'none';
+        return;
+    }
+    addContainer.style.display = 'flex';
+});
