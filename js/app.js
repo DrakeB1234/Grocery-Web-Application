91b30c4f1970861757bba081fd563a5f8a1c55f4
Drake Buentello <dbuentello211607@gmail.com>
2022-10-28 10:51:09 -0500

    added mobile nav bar, fixing adder container

--- js/app.js
+++ js/app.js
@@ -10,10 +10,12 @@ const introductionHide = () => {
     // get classes of respected elements
     const introElement = document.querySelector('.introduction-container');
     const mainElement = document.querySelector('.main-container-grid');
+    const navElement = document.querySelector('.navbar-mobile');
     // remove show class, add hide class to intro div
     introElement.classList.add("hide-intro");
     // remove hide class, add show class to main div
     mainElement.classList.remove("hide-main");
+    navElement.classList.remove("hide-intro");
     // this removes the event listener from window
     window.document.removeEventListener('scroll', introductionHide);
 }
