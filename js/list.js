638bb9167c7e8ca11b067d4f0235b3da83a39fd0
Drake Buentello <dbuentello211607@gmail.com>
2022-11-02 18:31:06 -0500

    added styling and functionality to adder container for grocery list

--- js/list.js
+++ js/list.js
@@ -1,36 +1,12 @@
-
 // hide/show add container
-document.getElementById('add-button').addEventListener('click', () => {
-    const addContainer = document.getElementById('input-container');
-    const addButton = document.getElementById('add-button');
-    if (addContainer.style.display == 'flex') {
-        addContainer.style.display = 'none';
-        addButton.classList.remove('rotate');
+document.getElementById('show-add-btn').addEventListener('click', () => {
+    const addContainer = document.getElementById('form-add');
+    const addBtn = document.getElementById('show-add-btn');
+    if (!(addContainer.classList.contains('show'))) {
+        addContainer.classList.add('show');
+        addBtn.classList.add('rotate');
         return;
     }
-    addButton.classList.add('rotate');
-    addContainer.style.display = 'flex';
-});
-
-// hide/show manage container
-document.getElementById('manage-button').addEventListener('click', () => {
-    const manageContainer = document.getElementById('manage-container');
-    const manageButton = document.getElementById('manage-button');
-    if (manageContainer.style.display == 'block') {
-        manageContainer.style.display = 'none';
-        manageButton.classList.remove('rotate');
-        return;
-    }
-    manageButton.classList.add('rotate');
-    manageContainer.style.display = 'block';
-});
-
-// hide/show add container
-document.getElementById('add-category-button').addEventListener('click', () => {
-    const addContainer = document.getElementById('form-add-category');
-    if (addContainer.style.display == 'flex') {
-        addContainer.style.display = 'none';
-        return;
-    }
-    addContainer.style.display = 'flex';
+    addBtn.classList.remove('rotate');
+    addContainer.classList.remove('show');
 });
