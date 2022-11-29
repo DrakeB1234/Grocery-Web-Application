22eddf8b0b1ce0c39191570c2d5160a2801d962c
drakebuentello <dbuentello211607@gmail.com>
2022-11-29 04:05:29 -0600

    added attributes to login form inputs

--- functions.py
+++ functions.py
@@ -10,7 +10,6 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            flash("Access Denied")
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
