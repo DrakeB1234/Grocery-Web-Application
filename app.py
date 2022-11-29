e0d0f6867428c6f023cf0b013b23fb45fe6d7297
drakebuentello <dbuentello211607@gmail.com>
2022-11-29 03:24:28 -0600

    added database connection, working on login system

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, redirect, request, render_template, flash
+from flask import Flask, redirect, request, render_template, flash, session
+from flask_mysqldb import MySQL
+from functions import login_required
+import re
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# database conn
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '791384265Templegrd'
+app.config['MYSQL_HOST'] = 'main-database.cbhkqg0xerfz.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_DB'] = 'database1'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
 
 """
     ~~~TODO~~~
@@ -12,11 +26,44 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # login page
 @app.route("/login", methods=["POST", "GET"])
+@login_required
 def login():
+    # establish database connection
+    db = mysql.connection.cursor()
     # user posted form
     if request.method == "POST":
-        flash(request.form.get("userName"))
-        flash(request.form.get("userPass"))
+        inputName = request.form.get("userName")
+        inputPass = request.form.get("userPass")
+
+        # check for valid inputs
+        if not inputName:
+            flash("Insert A Name")
+            return redirect("/login")
+        if not inputPass:
+            flash("Insert A Password")
+            return redirect("/login")
+        if not inputName.isalnum():
+            flash("Use Only Letters and Numbers")
+            return redirect("/login")
+        if not inputName.isalnum():
+            flash("Use Only Letters and Numbers")
+            return redirect("/login")
+        if not re.match("^[A-Za-z0-9$%#@!]*$",inputPass):
+            flash("Only use Letters, Numbers and ($, %, #, @, !)")
+            return redirect("/login")
+        
+        # fetch all users
+        db.execute('''SELECT * FROM users''')
+        rows = db.fetchall()
+        # check to see if name and pass match
+        for i in rows:
+            if i["username"] == inputName and i["hash"] == inputPass:
+                # if match, redirect to homepage and set session variable 
+                flash("Logged In")
+                session["user_id"] = i["user_id"]
+                flash(session["user_id"])
+                return redirect("/")
+        flash("Invalid username/password")
         return render_template("login.html")
 
     # user made GET request for page
--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ def login_required():
         Ensure that route is only accessible to those
         that are logged into an account
     """
-    if session["user_id"] = None:
-        return False
-    return True
+    if session["user_id"] == None:
+        return render_template("login.html")
+    else:
+        return redirect("/")
