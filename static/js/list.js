e20d7591a33691ac6fdf293a10d68cd239101f20
drakebuentello <dbuentello211607@gmail.com>
2022-12-01 08:37:09 -0600

    made some changes to JS code for list data, and styling

--- static/js/list.js
+++ static/js/list.js
@@ -7,14 +7,20 @@
         let curCat = e["category"];
         // if cur cat is different than past
         if (curCat != pastCat) {
+            html += `</div>`;
             html += `<div class='list-item'><h1>${curCat}</h1>`;
-        }
-        else {
-            html += "<div class='list-item'>";
         }
 
         // print each item in format
-        html += `<div class='item'><h2>${e["amount"]}</h2><h3>${e["item"]}</h3><h4>${e["note"]}</h4></div></div>`;
+        html += `<div class='item'><h2>${e["amount"]}</h2><h3>${e["item"]}</h3><i class="fa-solid fa-trash-can"></i>`;
+        // if there is a note, print h4 tag with it
+        if (e["note"] != "") {
+            html += `</div><h4>Note: ${e["note"]}</h4>`;
+        }
+        else {
+            html += `</div>`;
+        }
+
         // set past cat
         pastCat = e["category"];
     });
