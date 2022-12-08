4151ff1d080b90a7920be5da9b8c998aa14b856a
drakebuentello <dbuentello211607@gmail.com>
2022-12-08 17:46:44 -0600

    added some changes to settings and mobile nav

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,13 +1,18 @@
 // function for toggling mobile nav bar
 window.onload = () => {
-    const navEle = document.getElementById("mobile-nav-container")
-    document.querySelectorAll("#toggle-mobile-nav").forEach(e => {
+    document.querySelectorAll("#set-btn").forEach(e => {
         e.addEventListener("click", (e) => {
-            if (navEle.style.display === "flex"){
-                navEle.style.display = "none";
+            // set the original target element
+            let nextEle = e.originalTarget
+            console.log(nextEle)
+            // if next element in DOM is hidden
+            if (nextEle.nextElementSibling.style.display == "flex") {
+                nextEle.nextElementSibling.style.display = "none";
+                nextEle = "";
                 return;
             }
-            navEle.style.display = "flex";
+            nextEle.nextElementSibling.style.display = "flex";
+            nextEle = "";
         });
     });
 };
