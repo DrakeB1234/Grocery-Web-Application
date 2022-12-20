aa488d4bbb207884227edf10ec2035365812a84c
drakebuentello <dbuentello211607@gmail.com>
2022-12-19 20:52:04 -0600

    working on recipe editer

---
+++ static/js/recipesuser.js
@@ -0,0 +1,7 @@
+// function for toggling settings nav
+document.querySelectorAll(".recipe-card").forEach(e => {
+    e.addEventListener("click", (e) => {
+        // if next element in DOM is hidden
+        console.log(e.target.parentElement)
+    });
+});
