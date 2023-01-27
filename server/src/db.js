7e4186c4e5c9aaced5299fec0c478044aa66013b
drakebuentello <dbuentello211607@gmail.com>
2023-01-27 14:45:06 -0600

    working on nodemailer for user authenication

--- server/src/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-
-module.exports = mysql.createConnection({
-    host: 'main-database.cbhkqg0xerfz.us-east-2.rds.amazonaws.com',
-    user: 'root',
-    password: '791384265Templegrd',
-    database: 'database1'
-})
